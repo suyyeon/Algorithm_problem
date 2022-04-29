04c1953da113222fb3566be66ed3a8fdced418a8
SUYEON <ch2oyeon@gmail.com>
2022-04-29 20:33:28 +0900

    Create 최대공약수와최소공배수.java

---
+++ BaekJoon/최대공약수와최소공배수.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class 최대공약수와최소공배수 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+
+		System.out.println(max(a,b));
+		System.out.println(min(a,b));
+		
+	}
+	
+	static int max(int a, int b) { //최대공약수 
+		if(a%b ==0) {
+			return b;
+		}
+		return max(b, a%b);
+	}
+	
+	static int min(int a, int b) { //최소공배수
+		return a*b / max(a,b);
+	}
+}
