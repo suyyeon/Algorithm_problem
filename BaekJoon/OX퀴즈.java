f45ab9505c2f0b0212e9071ca39ca1ceea3348c5
SUYEON <ch2oyeon@gmail.com>
2022-04-30 23:45:22 +0900

    8958

---
+++ BaekJoon/OX퀴즈.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class OX퀴즈 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int input = scan.nextInt();
+		String list[] = new String[input];
+
+		for (int i = 0; i < input; i++) {
+			list[i] = scan.next();
+		}
+
+		for (int i = 0; i < input; i++) {
+			char[] list2 = list[i].toCharArray();
+			int count = 0;
+			int sum = 0;
+			for (int j = 0; j < list2.length; j++) {
+				if (list2[j] == 'O') {
+					count++;
+				} else {
+					count = 0;
+				}
+				sum = sum + count;
+			}
+			System.out.println(sum);
+			count = 0;
+			sum = 0;
+		}
+	}
+}
