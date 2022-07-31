c08c74eeaf184e08c2a4085b3495c484cdf01bb3
SUYEON <ch2oyeon@gmail.com>
2022-07-31 21:54:18 +0900

    데이터형

---
+++ Codeup/q1028.java
@@ -0,0 +1,15 @@
+package com.codeup.data;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q1028 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+        System.out.println(n);
+    }
+}
---
+++ Codeup/q1030.java
@@ -0,0 +1,14 @@
+package com.codeup.data;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q1030 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+        System.out.println(n);
+    }
+}
