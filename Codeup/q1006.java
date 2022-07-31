d8d36d7716b2cb6a9391ff7db4b83644a6697a98
SUYEON <ch2oyeon@gmail.com>
2022-07-31 21:56:11 +0900

    출력

---
+++ Codeup/q1006.java
@@ -0,0 +1,7 @@
+package com.codeup.output;
+
+public class q1006 {
+    public static void main(String[] args) {
+        System.out.println("\"!@#$%^&*()\"");
+    }
+}
---
+++ Codeup/q1008.java
@@ -0,0 +1,9 @@
+package com.codeup.output;
+
+public class q1008 {
+    public static void main(String[] args) {
+        System.out.print("\u250C\u252C\u2510\n" +
+                "\u251C\u253C\u2524\n" +
+                "\u2514\u2534\u2518");
+    }
+}
