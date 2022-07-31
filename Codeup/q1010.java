4ceb95c31a908934ce05aa87527e7d34df04c96d
SUYEON <ch2oyeon@gmail.com>
2022-07-31 21:54:57 +0900

    입출력

---
+++ Codeup/q1010.java
@@ -0,0 +1,13 @@
+package com.codeup.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q1010 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n);
+    }
+}
